29942fae0e038face7a6b746265ecb0fb981565c
mohit bang <63931979+mohitbang99@users.noreply.github.com>
2020-04-26 07:40:18 +0530

    Add files via upload

---
+++ file handling.py
@@ -0,0 +1,38 @@
+f=open('first1.txt',"r")
+print(f.read())
+f.close()
+f=open('first1.txt',"r")
+print(f.readline())
+print(f.readline())
+print(f.readline())
+F=open('first1.txt',"w")
+F.write("overwritten")
+F.close()
+F=open('first1.txt',"r")
+print(F.read())
+F.close()
+
+
+f=open("star.txt","r")
+print(f.read())
+f.close()
+
+
+try:
+    f=open("star.txt","r")
+
+except IOError:
+    print("file does not exist")
+else:
+    print(f.read())
+f.close()
+
+
+try:
+    f=open("steps.txt","r")
+
+except IOError:
+    print("file does not exist")
+else:
+    print(f.read())
+f.close()
